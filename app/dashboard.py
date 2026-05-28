bc953717535504d86a4bf3bf94fd1a1a6aeef0ec
zainiqbalsaputra-ux <zainiqbalsaputra@gmail.com>
2026-05-28 15:52:13 +0700

    Add realtime YOLO video detection

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import cv2
+
+from app.detection import detect_vehicles
+
 
 def run_dashboard():
+
     st.set_page_config(
         page_title="InvGuard AI",
         layout="wide"
@@ -8,3 +13,32 @@ def run_dashboard():
 
     st.title("🚦 InvGuard AI Dashboard")
     st.write("AI-powered traffic monitoring system")
+
+    # Video path
+    video_path = "videos/thailand.mp4"
+
+    cap = cv2.VideoCapture(video_path)
+
+    frame_placeholder = st.empty()
+
+    while cap.isOpened():
+
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        annotated_frame, results = detect_vehicles(frame)
+
+        annotated_frame = cv2.cvtColor(
+            annotated_frame,
+            cv2.COLOR_BGR2RGB
+        )
+
+        frame_placeholder.image(
+            annotated_frame,
+            channels="RGB",
+            use_container_width=True
+        )
+
+    cap.release()
--- app/detection.py
+++ app/detection.py
@@ -1,9 +1,31 @@
 from ultralytics import YOLO
 
-# Load model sekali saja
 model = YOLO("yolov8n.pt")
 
+# Kendaraan yang ingin dideteksi
+VEHICLE_CLASSES = [2, 3, 5, 7]
+# car, motorcycle, bus, truck
+
+
 def detect_vehicles(frame):
+
     results = model(frame)
 
-    return results
+    filtered_boxes = []
+
+    for result in results:
+
+        boxes = result.boxes
+
+        for box in boxes:
+
+            cls = int(box.cls[0])
+
+            conf = float(box.conf[0])
+
+            if cls in VEHICLE_CLASSES and conf > 0.5:
+                filtered_boxes.append(box)
+
+    annotated_frame = results[0].plot()
+
+    return annotated_frame, filtered_boxes
